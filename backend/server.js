2c2e540eb809ab2041cdea1980dbcc3fbd23157c
Nikisha Shrestha <snikisha002@gmail.com>
2026-08-14 02:10:15 +0000

    CI half v5

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+console.log ('Starting server...');
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const todoRoutes = require('./routes/todoRoutes');
+
 
 const app = express();
 
