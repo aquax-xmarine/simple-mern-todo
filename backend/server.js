7056c2834725b5d714302ef189b16425e0c9cb24
Nikisha Shrestha <snikisha002@gmail.com>
2026-08-14 02:40:12 +0000

    CI/CD

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ console.log ('Starting server 3...');
 console.log ('Starting server 4...');
 console.log ('Starting server 5...');
 console.log ('Starting server 6...');
+console.log ('Starting server 7...');
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
